308c6f7b48e147d32bc4f0a69f6464c9ed2786da
Cuccku <Cuccku@Cuckkus-Mac-mini.local>
2012-09-12 00:50:24 +0700

    ADD

---
+++ TESST/TESST/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TESST
+//
+//  Created by Cuccku on 1/5/11.
+//  Copyright (c) 2012 Cuccku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TESST/TESST/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TESST
+//
+//  Created by Cuccku on 1/5/11.
+//  Copyright (c) 2012 Cuccku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
